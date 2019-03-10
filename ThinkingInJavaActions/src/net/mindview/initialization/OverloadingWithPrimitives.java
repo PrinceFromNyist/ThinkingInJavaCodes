baa44d293b9dcda7a087d5a026dda2e075686700
zengqiang.wang <zengqiang.wang@qunar.com>
2019-03-10 22:03:11 +0800

    学习了方法重载在基本数据类型的体现，而且知道了数据类型可以从低类型自动提升为高类型，而反过来则需要强制转换。

---
+++ ThinkingInJavaActions/src/net/mindview/initialization/OverloadingWithPrimitives.java
@@ -0,0 +1,160 @@
+package net.mindview.initialization;
+
+
+import static net.mindview.utils.PrintUtils.*;
+/**
+ * 
+ * @version:v1.0
+ * @Description:测试方法重载在基本数据类型之间的体现,总结：在方法重载时，类型小的变量会自动转化为类型大的变量，比如：0调用testInt时，会转化为long，float和double类型
+ * @author:Prince
+ * @date:2019年3月10日 上午11:27:55
+ */
+public class OverloadingWithPrimitives {
+	public void f1(char c) {
+		println("f1(char) " + c);
+		
+	}
+	public void f1(byte b) {
+		println("f1(byte) " + b);
+	}
+	public void f1(int i) {
+		println("f1(int) " + i);
+	}
+	public void f1(short s) {
+		println("f1(short) " + s);
+	}
+	public void f1(long l) {
+		println("f1(long) " + l);
+	}
+	public void f1(float f) {
+		println("f1(float) " + f);
+	}
+	public void f1(double d) {
+		println("f1(double) " + d);
+	}
+	
+	
+	public void f2(byte b) {
+		println("f2(byte) " + b);
+	}
+	public void f2(int i) {
+		println("f2(int) " + i);
+	}
+	public void f2(short s) {
+		println("f2(short) " + s);
+	}
+	public void f2(long l) {
+		println("f2(long) " + l);
+	}
+	public void f2(float f) {
+		println("f2(float) " + f);
+	}
+	public void f2(double d) {
+		println("f2(double) " + d);
+	}
+	
+	public void f3(int i) {
+		println("f3(int) " + i);
+	}
+	public void f3(short s) {
+		println("f3(short) " + s);
+	}
+	public void f3(long l) {
+		println("f3(long) " + l);
+	}
+	public void f3(float f) {
+		println("f3(float) " + f);
+	}
+	public void f3(double d) {
+		println("f3(double) " + d);
+	}
+	
+	public void f4(short s) {
+		println("f4(short) " + s);
+	}
+	public void f4(long l) {
+		println("f4(long) " + l);
+	}
+	public void f4(float f) {
+		println("f4(float) " + f);
+	}
+	public void f4(double d) {
+		println("f4(double) " + d);
+	}
+	
+	public void f5(long l) {
+		println("f5(long) " + l);
+	}
+	public void f5(float f) {
+		println("f5(float) " + f);
+	}
+	public void f5(double d) {
+		println("f5(double) " + d);
+	}
+	
+	
+	public void f6(float f) {
+		println("f6(float) " + f);
+	}
+	public void f6(double d) {
+		println("f6(double) " + d);
+	}
+	
+	
+	
+	public void f7(double d) {
+		println("f7(double) " + d);
+	}
+	
+	public void testConstant() {
+		print("test constant:");
+		f1(5);f2(5);f3(5);f4(5);f5(5);f6(5);f7(5);
+	}
+	public void testChar(char c) {
+		print("test char:");
+		f1(c);f2(c);f3(c);f4(c);f5(c);f6(c);f7(c);
+	}
+	public void testByte(byte b) {
+		print("test char:");
+		f2(b);f3(b);f4(b);f5(b);f6(b);f7(b);
+	}
+	public void testShort(short s) {
+		print("test short:");
+		f3(s);f4(s);f5(s);f6(s);f7(s);
+	}
+	public void testInt(int i) {
+		print("test int:");
+		f4(i);f5(i);f6(i);f7(i);
+	}
+	public void testLong(long l) {
+		print("test long:");
+		f5(l);f6(l);f7(l);
+	}
+	public void testFloat(float f) {
+		print("test float:");
+		f6(f);f7(f);
+	}
+	public void testDouble(double d) {
+		print("test long:");
+		f7(d);
+	}
+	
+	
+	
+	public static void main(String[] args) {
+		OverloadingWithPrimitives p = new OverloadingWithPrimitives();
+		byte b = 0;
+		short s = 0;
+		p.testConstant();
+		p.testChar('c');
+		p.testByte(b);
+		p.testShort(s);
+		p.testInt(0);
+		p.testLong(0);
+		p.testFloat(0);
+		p.testDouble(0);
+
+
+	}
+
+}
---
+++ ThinkingInJavaActions/src/net/mindview/utils/PrintUtils.java
@@ -0,0 +1,27 @@
+package net.mindview.utils;
+/**
+ * 
+ * @version:v1.0
+ * @Description:自己封装的输出类
+ * @author:Prince
+ * @date:2019年3月10日 上午11:29:57
+ */
+public class PrintUtils {
+	/**
+	 * 换行输出
+	 * @Description:
+	 * @param o
+	 */
+	public static void println(Object o) {
+		System.out.println(o);
+	}
+	
+	/**
+	 * 不换行输出
+	 * @Description:
+	 * @param o
+	 */
+	public static void print(Object o) {
+		System.out.print(o);
+	}
+}
